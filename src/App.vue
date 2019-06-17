01238c18a3bbaec33b11b57525f35f3bb8d0a2ab
Rosen Kanev <rkunev@gmail.com>
2019-06-17 17:44:53 +0300

    fix: Remove debug ColorPicker config while developing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <color-picker v-bind="color" @input="onInput" variant="persistent" :initiallyCollapsed="true" />
+    <color-picker v-bind="color" @input="onInput" />
 </template>
 
 <script>
