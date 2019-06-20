dade5c615cf10968d700b959563677d6d7c82535
Rosen Kanev <rkunev@gmail.com>
2019-06-20 17:15:10 +0300

    build: Automate docs deploy

---
+++ scripts/deploy-docs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+yarn docs:build
+cd docs/.vuepress/dist
+
+git init
+git add -A
+
+author=$1
+
+if [[ -n "$author" ]]; then
+    git config user.email "$author"
+fi
+
+git commit -m 'deploy'
+
+git push -f git@github.com:radial-color-picker/vue-color-picker.git master:gh-pages
+
+cd -
